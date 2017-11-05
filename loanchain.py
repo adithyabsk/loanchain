2cb4703956edabd57e5d364d71540717a476b97e
Cloud User <ec2-user@ip-172-31-58-26.ec2.internal>
2017-11-05 00:42:42 +0000

    Very very basic front end, setup API details, and frozen requirments

--- loanchain.py
+++ loanchain.py
@@ -1,6 +1,24 @@
 from flask import Flask
+from flask import request
+from flask import render_template
+import reqeusts
+
 app = Flask(__name__)
 
 @app.route('/')
-def hello_world():
-    return 'Hello, World!'
+def index():
+    return render_template("index.html")
+
+@app.route('/bank/')
+def bank():
+    return render_template("bank.html")
+
+@app.route('/bank/', methods=['POST'])
+def bank_post():
+    text = request.form['text']
+    processed_text = text.upper()
+
+    return processed_text
+
+if __name__ == "__main__":
+    app.run()
